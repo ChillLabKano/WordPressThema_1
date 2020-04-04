1d4691069ff6d057668230aedb75e06ced17296c
ChillLabKano <chillstd.kano@gmail.com>
2020-04-04 22:57:43 +0900

    front-page.php作成、style.css読み込みを追加
    1. ./style.cssは初期設定だと読み込まれない仕様なので
    FUNCTION.PHPでSTYLE.CSSを読み込むようにした.
    2. FRONT-PAGE.PHPはWPが最初に探すファイル＝TOPページになる

--- functions.php
+++ functions.php
@@ -20,6 +20,11 @@ function theme_enqueue_styles() {
 	// Get the theme data
 	$the_theme = wp_get_theme();
     wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
+    // road ./style.css
+    if (!is_admin()) {
+        $the_theme = wp_get_theme();
+        wp_enqueue_style( 'child-understrap-custom-styles', get_stylesheet_directory_uri() . '/style.css', [], $the_theme->get( 'Version' ) );
+    }
     wp_enqueue_script( 'jquery');
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
